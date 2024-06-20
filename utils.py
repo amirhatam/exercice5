79688629b8cfc7d1ab6386caf68271c374ba67c6
Amir <amirhatam136@gmail.com>
2024-06-20 10:58:57 +0200

    updated utile

--- utils.py
+++ utils.py
@@ -1,13 +1,5 @@
 def calculate_average(numbers):
-  """
-  Calcule la moyenne d'une liste de nombres.
-
-  Args:
-    numbers: Une liste de nombres.
-
-  Returns:
-    La moyenne des nombres dans la liste ou 0 si la liste est vide.
-  """
+ 
   if not numbers:
     return 0
   return sum(numbers) / len(numbers)
